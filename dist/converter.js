96d2a2eba4528de04388cbf8d6ba34afd0f45fe2
Queenshiba <haruka0111.2a@gmail.com>
2020-07-08 01:48:25 +0900

    first commit

---
+++ dist/converter.js
@@ -0,0 +1,21 @@
+let originalHeight = 2600
+let originalwidth = 1000
+
+function findAspectRatio(height, width) {
+    widthAspectRatio = (width / height).toFixed(2);
+    heightAspectRatio = 1
+    return heightAspectRatio + ':' + widthAspectRatio
+}
+console.log(findAspectRatio(originalHeight, originalwidth))
+
+function setHeightGetWidth(heightAspectRatio, widthAspectRatio, newHeight) {
+    return newHeight * widthAspectRatio
+
+}
+console.log(setHeightGetWidth(heightAspectRatio, widthAspectRatio, 3000))
+
+function setWidthGetHeight(heightAspectRatio, widthAspectRatio, newWidth) {
+    return newWidth / widthAspectRatio
+
+}
+console.log(setWidthGetHeight(heightAspectRatio, widthAspectRatio, 1900))
